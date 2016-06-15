6f6c0f84bd220f5a39bdb2dbb84aa4e5e8d69f4c
knight <361714571@qq.com>
2016-06-16 07:35:44 +0800

    test

--- src/main/java/com/knightNote/utils/HttpUtils.java
+++ src/main/java/com/knightNote/utils/HttpUtils.java
@@ -5,7 +5,6 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,26 +26,50 @@ public class HttpUtils {
         String str;
         //创建一个webclient
         WebClient webClient = new WebClient();
+
 //        //htmlunit 对css和javascript的支持不好，所以请关闭之
 //        webClient.getOptions().setCssEnabled(false);
+//        webClient.getOptions().setUseInsecureSSL(true);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+//        webClient.getCookieManager().setCookiesEnabled(true);
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-//        webClient.waitForBackgroundJavaScript(10000);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.waitForBackgroundJavaScript(1000);
+        webClient.waitForBackgroundJavaScriptStartingBefore(1000);
         //获取页面
         HtmlPage page = null;
+        int times =10;
         try {
             page = webClient.getPage(url);
-            synchronized ( page ){
-                page.wait(10000);
+
+            for (int i = 0; i < times ; i++) {
+                synchronized ( page ){
+                    page.wait(2000);
+                }
+                if(page.getByXPath("//object")!=null){
+                    System.out.println("***************************yes**********************");
+                    for (int j = 0; j < page.getByXPath("//object").size(); j++) {
+                        System.out.println(page.getByXPath("//object").get(j));
+                    }
+                    break;
+                }
+                System.out.println("***************************no***************************");
 
             }
-        } catch (IOException | InterruptedException e) {
+
+        } catch (Exception  e) {
             e.printStackTrace();
         }
         //获取页面的TITLE
 //        str = page.getTitleText();
 //        System.out.println(str);
 //        //获取页面的XML代码
-        System.out.println(page.asXml());
+//        System.out.println("***************************start***************************");
+//        System.out.println(page.asXml());
+
+
+//        System.out.println("***************************end***************************");
+
 //        //获取页面的文本
 //        str = page.asText();
 //        System.out.println(str);
@@ -55,7 +78,7 @@ public class HttpUtils {
     public static void main(String[] args) {
 //        System.out.println(request("http://www.douyu.com/32892"));
 
-        homePage("http://www.douyu.com/32892");
+        homePage("http://www.douyu.com/312410");
 
 
     }
